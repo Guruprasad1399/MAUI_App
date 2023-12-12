e33af2568cde32234775ebf1240badd25e98ed7c
Guruprasad1399 <vgp1399@gmail.com>
2023-12-12 00:26:17 -0500

    Implement Enhanced Bluetooth Device Connection Workflow

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,85 @@
+﻿using Plugin.BLE;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Abstractions.Exceptions;
+using System.Collections.ObjectModel;
+using Microsoft.Maui.Dispatching;
+using System.Linq;
+using Plugin.BLE.Abstractions;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        private IAdapter adapter;
+        private IDevice connectedDevice;
+        public ObservableCollection<IDevice> Devices { get; private set; }
+
+        public MainPage()
+        {
+            InitializeComponent();
+            adapter = CrossBluetoothLE.Current.Adapter;
+            Devices = new ObservableCollection<IDevice>();
+            devicesList.ItemsSource = Devices;
+        }
+
+        private async Task ScanForDevicesAsync()
+        {
+            Devices.Clear();
+            adapter.DeviceDiscovered += (s, a) =>
+            {
+                MainThread.BeginInvokeOnMainThread(() =>
+                {
+                    Devices.Add(a.Device);
+                });
+            };
+            await adapter.StartScanningForDevicesAsync();
+        }
+
+        private async void OnDeviceSelected(object sender, SelectionChangedEventArgs e)
+        {
+            if (e.CurrentSelection.FirstOrDefault() is IDevice device)
+            {
+                try
+                {
+                    await adapter.ConnectToDeviceAsync(device);
+                    connectedDevice = device;
+
+                    if (device.State == DeviceState.Connected)
+                    {
+                        connectionStatusLabel.Text = $"Connected to {device.Name}";
+                        bluetoothButton.Text = $"Search for Devices again and disconnect from {device.Name}";
+                        devicesList.IsVisible = false;
+                    }
+                    else
+                    {
+                        connectionStatusLabel.Text = "Connection failed";
+                        devicesList.IsVisible = true;
+                    }
+                }
+                catch (DeviceConnectionException ex)
+                {
+                    connectionStatusLabel.Text = "Connection error";
+                    devicesList.IsVisible = true;
+                    await DisplayAlert("Error", $"Connection failed: {ex.Message}", "OK");
+                }
+            }
+        }
+
+        private async void OnBluetoothButtonClicked(object sender, EventArgs e)
+        {
+            if (connectedDevice != null)
+            {
+                var disconnectAlert = DisplayAlert("Disconnecting", $"Disconnecting from {connectedDevice.Name}...", "OK");
+                await adapter.DisconnectDeviceAsync(connectedDevice);
+                await disconnectAlert;
+
+                connectedDevice = null;
+                connectionStatusLabel.Text = "Disconnected";
+                bluetoothButton.Text = "Scan for Devices";
+            }
+
+            await ScanForDevicesAsync();
+            devicesList.IsVisible = true;
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Bluetooth Connectivity App in .NET MAUI
+
+## Overview
+This project is a prototype of a .NET MAUI application designed just to showcase the ability of MAUI in developing a Windows app to use Bluetooth connectivity. It's an example of leveraging .NET MAUI's networking capabilities to detect and respond to changes in Bluetooth connection status. It is also designed to solve the challenge that react native poses for Windows App.
+
+## Features
+- Monitors network connectivity.
+- Detects Bluetooth connection status.
+- Logs connectivity changes to the console.
+
+## Prerequisites
+- .NET 6 SDK or later.
+- Visual Studio 2022 with the .NET MAUI workload installed.
+
+## Getting Started
+1. Clone the repository to your local machine.
+2. Open the solution in Visual Studio.
+3. Build and run the application to see Bluetooth connectivity monitoring in action in the Windows App.
